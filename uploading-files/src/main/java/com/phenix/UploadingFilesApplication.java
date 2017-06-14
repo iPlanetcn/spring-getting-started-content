791a6434713ec5b6b191aada007ea42cdd0f63d4
john <iplanetcn@gmail.com>
2017-06-14 17:26:29 +0800

    add uploading files

---
+++ uploading-files/src/main/java/com/phenix/UploadingFilesApplication.java
@@ -0,0 +1,26 @@
+package com.phenix;
+
+import com.phenix.storage.StorageProperties;
+import com.phenix.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
